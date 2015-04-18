81d38002307bd314ae113a96ad34be4fbe994d49
Mallory Bucell <m.f.bucell@gmail.com>
2015-04-18 15:46:25 -0400

    fix typo in php for email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,7 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
+   // empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -20,7 +20,7 @@ $to = 'mallorybucelldotcom@gmail.com'; // Add your email address inbetween the '
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: mallory@maillorybucell.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers = "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
