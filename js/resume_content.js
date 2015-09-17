523a9b431f288c471f9028ca632e6f730417f107
Mallory Bucell <m.f.bucell@gmail.com>
2015-09-16 23:04:37 -0400

    planning hash obj content

--- js/resume_content.js
+++ js/resume_content.js
@@ -1,7 +1,13 @@
-<<<<<<< Updated upstream
-var content = {
-=======
 var resumeEvents = {
->>>>>>> Stashed changes
 
 };
+
+/* conferences: railsconf, rubynation, volutnteer at CodeHer, CodeforDC, TechLadyHackathon, Girl Develop It TA */
+
+/* education, RA exp, TA Exp, ESL volunteer */
+
+/* work dacor, work prudential, work AIR */
+
+/* add tag obj { "tags": ["tag1", "tag2", "etc"] }
+
+/*todo: readme with possible conversion to redis or other key-value store */
